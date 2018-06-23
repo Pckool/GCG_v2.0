1881860d816874457692a1dc273252fbf647dd56
Philippe <phillclesco@yahoo.com>
2018-06-22 20:07:29 -0400

    Grab functionality for all platforms
    Added the same logic to the other two buttons!

--- src/js/home.js
+++ src/js/home.js
@@ -3,7 +3,7 @@ const util = require('util');
 import copy from 'copy-to-clipboard';
 
 app.controller('homeController', function($scope){
-    $scope.slider = document.getElementById("numCodes");
+    $scope.slider = document.getElementById("numCodes"); // Sets th slider to a value in $scope
     $scope.sliderValue = parseInt($scope.slider.value); // Display the slider value
 
     // Update the current slider value (each time you drag the slider handle)
@@ -14,7 +14,6 @@ app.controller('homeController', function($scope){
 
     $scope.pcCodeGrab = function(){
         try{
-
             var codez = '';
             var newList = '';
             fs.readFile(config['pc'], function(err, text){
@@ -36,6 +35,68 @@ app.controller('homeController', function($scope){
                 console.log(codez);
                 copy(codez);
                 jetpack.write(config['pc'], newList.trim());
+                counter = 0;
+            });
+
+        } catch(err){
+            console.log('Something went wrong: ' + err);
+        }
+    }
+    $scope.ps4CodeGrab = function(){
+        try{
+
+            var codez = '';
+            var newList = '';
+            fs.readFile(config['ps4'], function(err, text){
+                console.log(text);
+                var counter = 1;
+                text.toString().split('\n').forEach(function(ln){
+                    if(err){
+                        throw err;
+                    }
+                    else if (counter <= $scope.sliderValue){
+                        console.log('Code ' + counter + ': ' + ln)
+                        codez += ln;
+                        counter++;
+                    }
+                    else if (counter > $scope.sliderValue){
+                        newList += ln + "\n";
+                    }
+                });
+                console.log(codez);
+                copy(codez);
+                jetpack.write(config['ps4'], newList.trim());
+                counter = 0;
+            });
+
+        } catch(err){
+            console.log('Something went wrong: ' + err);
+        }
+    }
+    $scope.pcCodeGrab = function(){
+        try{
+
+            var codez = '';
+            var newList = '';
+            fs.readFile(config['xb1'], function(err, text){
+                console.log(text);
+                var counter = 1;
+                text.toString().split('\n').forEach(function(ln){
+                    if(err){
+                        throw err;
+                    }
+                    else if (counter <= $scope.sliderValue){
+                        console.log('Code ' + counter + ': ' + ln)
+                        codez += ln;
+                        counter++;
+                    }
+                    else if (counter > $scope.sliderValue){
+                        newList += ln + "\n";
+                    }
+                });
+                console.log(codez);
+                copy(codez);
+                jetpack.write(config['xb1'], newList.trim());
                 counter = 0;
             });
 
