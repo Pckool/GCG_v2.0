ab5426381d33de5029fb848370b81a7d2c50de1c
Philippe <phillclesco@yahoo.com>
2018-07-05 12:38:05 -0400

    More Cleanup

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const createWindow = () => {
 		backgroundColor: '#3B414F',
         frame: true,
         autoHideMenuBar: true,
-        icon: path.join(__dirname, '/media/logo/gcg_icon.ico'),
+        icon: path.join(__dirname, '/media/logo/gcg_icon_.ico'),
 	});
 
 	// and load the index.html of the app.
